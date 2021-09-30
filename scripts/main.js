c9d5f8b9873bf60506542a16c889f8ffc3a6d041
Ruan Felipe Sasse <ruansasse7@gmail.com>
2021-09-29 22:14:01 -0300

    cabeçalho  com toggle

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+/*Abre fecha menu*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show') /*tira e coloca*/
+  })
+}
+
+/*clicar no itens do menu e fechar o menu*/
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
