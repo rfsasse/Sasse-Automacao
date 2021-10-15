b4b70bc2f978530e77b52b1c243126d3b71ccf25
rfsasse <ruansasse7@gmail.com>
2021-10-14 22:08:45 -0300

    scroll js

--- scripts/main.js
+++ scripts/main.js
@@ -19,6 +19,20 @@ for (const link of links) {
   })
 }
 
+/*Mudar o header da pag quando fazer scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    //maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que altura do header
+    header.classList.remove('scroll')
+  }
+}
+
 /*Swiper*/
 
 const swiper = new Swiper('.swiper', {
@@ -36,3 +50,61 @@ const swiper = new Swiper('.swiper', {
   }
 })
 
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #products header, #products .product
+  #contact .text, #contact .links,
+  #contact .title, 
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+
+
+/*Menu conforme seção visivel na pagina*/
+const sections =
+  document.querySelectorAll(
+    'main section[id]'
+  )
+  
+/*Pega todos os ids dentro das seções que estão dentro do main*/
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+
+/*when scroll*/
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
